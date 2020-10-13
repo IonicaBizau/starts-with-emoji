47e884b22061f9d4cb60727813da24b1501faec3
Nabeel Ahmed <nabeelahmed1721@gmail.com>
2020-10-13 18:14:32 -0400

    added fix: only returns string or undefined

--- test/index.js
+++ test/index.js
@@ -5,16 +5,9 @@ const tester = require("tester")
 tester.describe("run the tests", test => {
     test.it("detect when starts with emoji", () => {
 
-        // Not sure how to make test objects
-        // test.expect(startsWithEmoji(":house: sweet home")).toBe({
-        //     startsWithEmoji: true, emoji: ':house:'
-        // });
-
-        test.expect(startsWithEmoji(":house: sweet home").startsWithEmoji).toBe(true);
-        test.expect(startsWithEmoji(":house: sweet home").emoji).toBe(':house:');
-        test.expect(startsWithEmoji("What a nice :gift:!").startsWithEmoji).toBe(false);
-        test.expect(startsWithEmoji("What a nice :gift:!").emoji).toBe(undefined);
-        test.expect(startsWithEmoji(":+1: foo").startsWithEmoji).toBe(true);
-        test.expect(startsWithEmoji(":+1: foo").emoji).toBe(':+1:');
+        test.expect(startsWithEmoji(":house: sweet home")).toBe(':house:');
+        test.expect(startsWithEmoji("What a nice :gift:!")).toBe(undefined);
+        test.expect(startsWithEmoji(":+1: foo")).toBe(':+1:');
+        test.expect(startsWithEmoji("hello world")).toBe(undefined);
     });
 });
